0bd6e91bb12c5b3bd1fc2b3e84c10ec1247e6f8f
webcogy <ungk456@nate.com>
2023-10-09 18:17:17 +0900

    v.1.0.0

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const scirptName = '고야호'
 /*********************************************************************************
  * LOA API 정보
  * 분당 100회 호출가능. 길드내 사용 용도라면 사실상 무제한. 그리고 무료.
@@ -9,16 +10,34 @@ const callMark = '/'
 const SP = '\n' // 다음 줄로 넘기기 (Spacing)
 
 // API
+/* 231009 웹 구문 => 안드로이드구문으로 변경
 const callApi = (url) => {
     return fetch(url,
         {
             method: 'GET',
             headers: {
                 'accept': 'application/json',
-                'authorization': `bearer ${apiKey}`,
+                'authorization': 'bearer '+ apiKey,
             },
     })
 }
+*/
+const callApi = (url) => {
+    try{
+        let response = org.jsoup.Jsoup.connect(url)
+        .header("Authorization", "Bearer " + apiKey)
+        .header("Content-Type", "application/json")
+        .ignoreContentType(true)
+        .get();
+
+        let data = JSON.parse(response.text());
+        return data
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
 /*********************************************************************************
  * 니나브 대사
 *********************************************************************************/
@@ -32,12 +51,6 @@ const Ninav = [
     '내가 지켜줄게 !',
     '전혀 성장하지 않았구나 쿠크세이튼 !',
 ]
-const callNinav = () => {
-    const leng = Ninav.length
-    const num = parseInt(Math.random(leng)*10)
-    return SP + SP + Ninav[num]
-}
-
 const NinavPretty = [
     'ദ്ദി*ˊᗜˋ*) 칭찬 고마워. 너도 이뻐',
     '(｡♥‿♥｡) 고마워. 나도 이쁜거 알고 있어',
@@ -52,6 +65,23 @@ const NinavCuty = [
     'ദ്ദി⑉¯ ꇴ ¯⑉ ) 아이참 ~ 너두 너무 귀여워',
     '(￣▽￣)ノ 역시 넌 보는 눈이 있네. 난 너무 귀여워',
 ]
+
+const callNinav = () => {
+    const leng = Ninav.length
+    const num = parseInt(Math.random()*leng)
+    return Ninav[num]
+}
+const callNinavPretty = () => {
+    const leng1 = NinavPretty.length
+    const num1 = parseInt(Math.random()*leng1)
+    return NinavPretty[num1]
+}
+const callNinavCuty = () => {
+    const leng2 = NinavCuty.length
+    const num2 = parseInt(Math.random()*leng2)
+    return NinavCuty[num2]
+}
+
 /*********************************************************************************
     ※ 메신저봇R 파라미터 정보
         string room: 방 이름
@@ -67,45 +97,49 @@ const NinavCuty = [
  *********************************************************************************/
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     if (msg == callMark + '테스트') {
-        getTest(replier)
+        getTest(room, replier)
     }
     if (msg == callMark + '기능') {
-        getAll(replier)
+        getAll(room, replier)
     }
-    if (msg == callMark + '도가토') {
-        getGuardianraids(replier)
+    if (msg == callMark + '주간') {
+        weeklyContent(room, replier)
     }
-    if (msg == callMark + '도어던') {
-        getAbyssdungeons(replier)
+    if (msg.includes(callMark + '정보')) {
+        let username = msg.split(' ')[1]
+        characterInformation(room, replier, username)
+    }
+    if (msg.includes('ㅂㅂ')) {
+        let callPrice = msg.split('ㅂㅂ')[1]
+        auctionPrice(room, replier, callPrice)
     }
 
     /* 니나브 */
-    if (
-        msg.includes('이쁨') &&
-        msg.includes('이뻐') &&
-        msg.includes('이쁘') &&
-        msg.includes('이뻤') &&
-        msg.includes('이쁜') &&
-        msg.includes('예쁨') &&
-        msg.includes('예뻐') &&
-        msg.includes('예쁘') &&
-        msg.includes('예쁜')
-    ) {
-        const leng = NinavPretty.length
-        const num = parseInt(Math.random(leng)*10)
-        replier.reply(NinavPretty[num])
+    if( msg.includes('니나브') ){
+        replier.reply(room, callNinav())
     }
     if (
-        msg.includes('귀염') &&
-        msg.includes('귀엽') &&
-        msg.includes('커여') &&
-        msg.includes('귀여') &&
-        msg.includes('기여') &&
+        msg.includes('이쁨') ||
+        msg.includes('이뻐') ||
+        msg.includes('이쁘') ||
+        msg.includes('이뻤') ||
+        msg.includes('이쁜') ||
+        msg.includes('예쁨') ||
+        msg.includes('예뻐') ||
+        msg.includes('예쁘') ||
+        msg.includes('예쁜')
+    ) {
+        replier.reply(room, callNinavPretty())
+    }
+    if (
+        msg.includes('귀염') ||
+        msg.includes('귀엽') ||
+        msg.includes('커여') ||
+        msg.includes('귀여') ||
+        msg.includes('기여') ||
         msg.includes('커엽')
     ) {
-        const leng = NinavCuty.length
-        const num = parseInt(Math.random(leng)*10)
-        replier.reply(NinavCuty[num])
+        replier.reply(room, callNinavCuty())
     }
 }
 
@@ -116,16 +150,18 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     @name 기능
     @function getAll
 
-    @name 도가토
-    @function getGuardianraids
+    @name 주간컨텐츠(도가토/도비스)
+    @function weeklyContent
+    
+    @name 정보(캐릭터)
+    @function characterInformation
 
-    @name 도어던
-    @function getAbyssdungeons
+    @name 경매가(분배금)
+    @function auctionPrice
 ******************************/
 
-function getTest(replier){
-    replier.reply(`테스트\n\n테스트입니다`)
-    // replier.reply(`테스트${S}${S}테스트입니다`)
+function getTest(room, replier){
+    replier.reply(room, '테스트\n\n테스트입니다')
 }
 
 /*****************************
@@ -134,71 +170,124 @@ function getTest(replier){
     @description 봇의 전체 기능 확인하기. 그냥 배열에 수동적으로 넣어서 뿌려주는거임.
 *****************************/
 const fn = [
-    callMark+'도가토',
-    callMark+'도어던',
+    '★봇 전체기능',
+    callMark+'주간',
+    callMark+'정보 아이디',
+    'ㅂㅂ금액',
 ]
-function getAll(replier){
+function getAll(room, replier){
     const combinedText = fn.map(item => item).join(SP);
-    replier.reply(combinedText)
+    replier.reply(room, combinedText)
 }
 
 /*****************************
-    @name 도가토
-    @function getGuardianraids
+    @name 주간컨텐츠(도가토/도비스)
+    @function weeklyContent
     @description
-    - 주간컨텐츠인 도전 가디언 토벌 정보
+    - 주간컨텐츠인 도가토, 도비스 정보
     - LOA API 정보 긁어옴
 *****************************/
-function getGuardianraids(replier){
-    const url = "https://developer-lostark.game.onstove.com/gamecontents/challenge-guardian-raids"
+function weeklyContent(room, replier){
+    const url1 = "https://developer-lostark.game.onstove.com/gamecontents/challenge-guardian-raids"
+    const url2 = "https://developer-lostark.game.onstove.com/gamecontents/challenge-abyss-dungeons"
 
-    callApi(url)
-        .then(response => response.json())
-        .then(data => {
-            const text = `
-                던전1: ${data.Raids[0].Name}, ${SP}
-                던전2: ${data.Raids[1].Name}, ${SP}
-                던전3: ${data.Raids[2].Name}, ${SP}
-            `
-            replier.reply(text)
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error)
-        })
+    let data1 = callApi(url1)
+    let data2 = callApi(url2)
+
+    const text = 
+        '★주간컨텐츠' + SP + SP +
+        '[도가토1] ' + data1.Raids[0].Name + ',' +  SP +
+        '[도가토2] ' + data1.Raids[1].Name + ',' +  SP +
+        '[도가토3] ' + data1.Raids[2].Name + SP + SP +
+        '[도비스1] ' + data2[0].Name+ ',' +  SP +
+        '[도비스2] ' + data2[1].Name;
+    
+    replier.reply(room, text)
 }
 
-/*****************************
-    @name 도어던
-    @function getAbyssdungeons
-    @description
-    - 주간컨텐츠인 도전 어디스 던전 정보
-    - LOA API 정보 긁어옴
-*****************************/
-function getAbyssdungeons(replier){
-    const url = "https://developer-lostark.game.onstove.com/gamecontents/challenge-abyss-dungeons"
 
-    callApi(url)
-        .then(response => response.json())
-        .then(data => {
-            const text = `
-                던전1: ${data[0].Name} ${SP}
-                보상아이템: [${data[0].RewardItems[0].Grade}] ${data[0].RewardItems[0].Name},
-                [${data[0].RewardItems[1].Grade}] ${data[0].RewardItems[1].Name},
-                [${data[0].RewardItems[2].Grade}] ${data[0].RewardItems[2].Name},
-                [${data[0].RewardItems[3].Grade}] ${data[0].RewardItems[3].Name},
-                [${data[0].RewardItems[4].Grade}] ${data[0].RewardItems[4].Name}
-                ${SP}
-                ${SP}
-                던전2: ${data[1].Name} ${SP}
-                보상아이템: [${data[1].RewardItems[0].Grade}] ${data[1].RewardItems[0].Name},
-                [${data[1].RewardItems[1].Grade}] ${data[1].RewardItems[1].Name},
-                [${data[1].RewardItems[2].Grade}] ${data[1].RewardItems[2].Name},
-                [${data[1].RewardItems[3].Grade}] ${data[1].RewardItems[3].Name},
-                [${data[1].RewardItems[4].Grade}] ${data[1].RewardItems[4].Name}
-            `
-            replier.reply(text)
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error)
-        })
+
+/*****************************
+    @name 정보(캐릭터)
+    @function characterInformation
+    @description 로아 캐릭터 정보
+*****************************/
+function characterInformation(room, replier, username) {
+    var defaultUrl = 'https://developer-lostark.game.onstove.com';
+    var encodedName = encodeURIComponent(username);
+    var url = defaultUrl + '/characters/' + encodedName + '/siblings';
+
+    var data = callApi(url);
+
+    var text = '';
+    var serverData = {}; // Create an object to group data by ServerName
+
+    for (var i = 0; i < data.length; i++) {
+        var currentServerName = data[i].ServerName;
+        
+        if (!serverData[currentServerName]) {
+            serverData[currentServerName] = []; // Initialize an array for each unique ServerName
+        }
+
+        // Remove commas from ItemAvgLevel
+        var itemAvgLevel = data[i].ItemAvgLevel.split(',').join('');
+
+        serverData[currentServerName].push({
+            CharacterClassName: data[i].CharacterClassName,
+            CharacterName: data[i].CharacterName,
+            ItemAvgLevel: parseFloat(itemAvgLevel).toFixed(0)
+        });
+    }
+
+    for (var serverName in serverData) {
+        if (serverData.hasOwnProperty(serverName)) {
+            // Sort the array of characters for each server based on ItemAvgLevel in descending order
+            serverData[serverName].sort(function (a, b) {
+                return b.ItemAvgLevel - a.ItemAvgLevel;
+            });
+
+            text += '★' + serverName + SP + SP;
+            
+            for (var j = 0; j < serverData[serverName].length; j++) {
+                var character = serverData[serverName][j];
+                text += '[' + character.CharacterClassName + '] ' + SP + 
+                        character.CharacterName + ' / ' +
+                        character.ItemAvgLevel + '레벨' + SP + SP;
+            }
+        }
+    }
+    
+    text = text.trim();
+
+    replier.reply(room, text);
+}
+
+
+
+/*****************************
+    @name 경매가(분배금)
+    @function auctionPrice
+    @description 경매 입찰 최적가 (4인, 8인)
+*****************************/
+function auctionPrice(room, replier, callPrice){
+
+    const cal_4 = (p) => {
+        return Math.floor(p * 0.95 * (3.0 / 4.0));
+    } 
+    const cal_8 = (p) => {
+        return Math.floor(p * 0.95 * (7.0 / 8.0))
+    }
+    
+    if (isNaN(cal_4(callPrice))) {
+            replier.reply(room, "금액 정확히 입력해'줘'");
+    }else{
+        let text = 
+            '[4인 입찰최적가]' + SP +
+            cal_4(callPrice) + '원' + SP + SP +
+            '[8인 입찰최적가]' + SP +
+            cal_8(callPrice) + '원';  
+
+        replier.reply(room, text); 
+    }
+    
 }
